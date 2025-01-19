03211f47a3da0b95dc8e421c278e39d83385faf3
Salino3 <flaviobcn012@gmail.com>
2025-01-19 10:41:37 +0100

    coomt

--- src/components/roma/data-roma.jsx
+++ src/components/roma/data-roma.jsx
@@ -60,18 +60,6 @@ export const DataRoma = () => {
     popupAnchor: [0, -10],
   });
 
-  //     iconUrl: `<svg
-  //   xmlns="http://www.w3.org/2000/svg"
-  //   viewBox="0 0 24 24"
-  //   width="24"
-  //   height="24"
-  //     // style={{ transform: 'rotate(${rotation}deg)' }}
-  // >
-  //   <polygon
-  //     points="5,19 12,12 19,19"
-  //     fill="green"
-  //   />
-  // </svg>`,
   const arrowIconRotation = (rotation) => {
     return divIcon({
       className: "arrow-icon",
